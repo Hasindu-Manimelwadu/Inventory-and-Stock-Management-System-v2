a130208056906929138c4bb67d10af3a12f4eac3
IT25102189 <it25102189@my.sliit.lk>
2026-05-15 17:32:41 +0530

    Added User abstract class

---
+++ src/main/java/com/example/usermanagementsystem/model/User.java
@@ -0,0 +1,66 @@
+package com.example.usermanagementsystem.model;
+
+public abstract class User {
+    private String userId;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email, String password, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public abstract String getPermissionLevel();
+
+    public String toFileString() {
+        return userId + "," + username + "," + email + "," + password + "," + role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
